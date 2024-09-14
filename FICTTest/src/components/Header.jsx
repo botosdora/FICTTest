72507fef1eee553161736bb0f2674d4e958a16a0
botosdora <botosdoraa@gmail.com>
2024-09-14 16:56:58 +0200

    completed header design

---
+++ FICTTest/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./../styles/header.css"
+
+export default function Header() {
+  return (
+    <header>
+        <p>Login</p>
+        <p>Register</p>
+        <p>Users</p>
+        <p>Searches</p>
+    </header>
+  )
+}
