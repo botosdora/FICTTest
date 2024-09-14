68ba9b248ea32affa7a44e486f2197823d7c2bb5
botosdora <botosdoraa@gmail.com>
2024-09-14 16:57:16 +0200

    completed login design

---
+++ FICTTest/src/routes/Login.jsx
@@ -0,0 +1,10 @@
+import LoginBox from "../components/LoginBox";
+//import RegisterBox from "../components/RegisterBox";
+
+export default function Login() {
+  return (
+    <>
+    <LoginBox />
+    </>
+  )
+}
