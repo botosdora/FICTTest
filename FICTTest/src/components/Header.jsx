6efda1509d29c1d359e02cdfa44997ec76a59090
botosdora <botosdoraa@gmail.com>
2024-09-15 18:14:20 +0200

    added logout btn

--- FICTTest/src/components/Header.jsx
+++ FICTTest/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import "./../styles/header.css"
 
 export default function Header() {
+
+  function handleLogout() {
+    localStorage.removeItem("loggedInUser")
+    alert("Successful logout!")
+  }
+
   return (
     <header>
-        <a href="/login">Login</a>
-        <a href="/register">Register</a>
+        <a href="/login">Login/register</a>
         <a href="/users">Users</a>
         <a href="/searches">Searches</a>
+        <button className="logout-button" onClick={handleLogout}>Logout</button>
     </header>
   )
 }
