9cf24c3523aa16fe7f0f9247a9e392df1cb7c912
botosdora <botosdoraa@gmail.com>
2024-09-15 16:25:22 +0200

    completed register function

--- FICTTest/src/components/RegisterBox.jsx
+++ FICTTest/src/components/RegisterBox.jsx
@@ -1,26 +1,104 @@
+import { useState } from "react";
 import "./../styles/register.css";
 
 export default function RegisterBox() {
+  const [formData, setFormData] = useState({
+    nickname: "",
+    fullname: "",
+    email: "",
+    password: "",
+    birthday: "",
+    height: "",
+    gender: "male",
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(formData)
+
+    const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+    if (existingUsers.some(user => user.email === formData.email)) {
+      alert("E-mail already in use.");
+      return;
+    }
+
+    const updatedUsers = [...existingUsers, formData];
+    localStorage.setItem("users", JSON.stringify(updatedUsers));
+
+    setFormData({
+      nickname: "",
+      fullname: "",
+      email: "",
+      password: "",
+      birthday: "",
+      height: "",
+      gender: "male",
+    });
+
+    alert("Successful registration!");
+  }
+
   return (
     <>
       <div className="container">
         <div className="form-box">
           <h2>Register</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <label htmlFor="nickname">Nickname</label>
-            <input type="text" id="nickname" name="nickname" required />
+            <input
+              type="text"
+              id="nickname"
+              name="nickname"
+              required
+              onChange={(e) =>
+                setFormData({ ...formData, nickname: e.target.value })
+              }
+              value={formData.nickname}
+            />
 
             <label htmlFor="fullname">Full Name</label>
-            <input type="text" id="fullname" name="fullname" required />
+            <input
+              type="text"
+              id="fullname"
+              name="fullname"
+              required
+              onChange={(e) =>
+                setFormData({ ...formData, fullname: e.target.value })
+              }
+              value={formData.fullname}
+            />
 
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" required />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              value={formData.email}
+            />
 
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+              value={formData.password}
+            />
 
             <label htmlFor="birthday">Birthday</label>
-            <input type="date" id="birthday" name="birthday" required />
+            <input type="date" id="birthday" name="birthday" required onChange={(e) =>
+                setFormData({ ...formData, birthday: e.target.value })
+              }
+              value={formData.birthday}
+              />
 
             <label htmlFor="height">Height (in cm)</label>
             <input
@@ -30,6 +108,10 @@ export default function RegisterBox() {
               required
               min="0"
               max="220"
+              onChange={(e) =>
+                setFormData({ ...formData, height: e.target.value })
+              }
+              value={formData.height}
             />
 
             <div className="gender-radios">
@@ -39,6 +121,10 @@ export default function RegisterBox() {
                 id="male"
                 name="gender"
                 value="male"
+                checked={formData.gender === "male"}
+                onChange={(e) =>
+                  setFormData({ ...formData, gender: e.target.value })
+                }
                 required
               />
               <label htmlFor="male">Male</label>
@@ -48,6 +134,10 @@ export default function RegisterBox() {
                 id="female"
                 name="gender"
                 value="female"
+                checked={formData.gender === "female"}
+                onChange={(e) =>
+                  setFormData({ ...formData, gender: e.target.value })
+                }
                 required
               />
               <label htmlFor="female">Female</label>
