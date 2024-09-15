6c379e68f47e827ff82228554424fae9294f1ddd
botosdora <botosdoraa@gmail.com>
2024-09-15 18:14:31 +0200

    created login session

--- FICTTest/src/routes/Login.jsx
+++ FICTTest/src/routes/Login.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import LoginBox from "../components/LoginBox";
+import RegisterBox from "../components/RegisterBox"
 
 export default function Login() {
+  const [isRegistering, setIsRegistering] = useState(true)
+
   return (
     <>
-    <LoginBox />
+    <button onClick={() => setIsRegistering(!isRegistering)} style={{marginBottom: "10px", width: "10rem"}}>
+      {isRegistering ? "Go to login." : "Go to register."}
+    </button>
+
+    {isRegistering ? <RegisterBox /> : <LoginBox />}
     </>
   )
 }
