f38e6660ceacc0cfb0db3885565586bd4a1ee8c1
botosdora <botosdoraa@gmail.com>
2024-09-15 18:14:40 +0200

    updated loginbox

--- FICTTest/src/components/LoginBox.jsx
+++ FICTTest/src/components/LoginBox.jsx
@@ -1,24 +1,59 @@
-import "./../styles/login.css"
+import { useState } from "react";
+import "./../styles/login.css";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginBox() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate()
+
+  function handleLogin(e) {
+    e.preventDefault()
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const user = users.find(user => user.email === email && user.password === password);
+
+    if (user) {
+      localStorage.setItem("loggedInUser", email);
+      alert (`Hello, ${user.fullname}`)
+    } else {
+      alert("Invalid data or non-existing account")
+    }
+    
+    navigate("/users")
+  }
+
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="form-box">
           <h2>Login</h2>
-          <form>
-
+          <form onSubmit={handleLogin}>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" required />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
             <button type="submit">Log in</button>
-            <p>Or register here!</p>
           </form>
         </div>
       </div>
     </>
-  )
+  );
 }
